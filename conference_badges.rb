759aeb262cbe427d7c6ff2dd6675cbfa4769b4f5
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-06 19:54:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,8 +23,13 @@ def assign_rooms(names)
 end
 
 def printer(names)
-  badges = batch_badge_creator(names)
-  badges.each do |badge|
-    puts badge
+  rooms = assign_rooms(names)
+  names.each do |name|
+    puts badge_maker(name)
   end
+
+  rooms.each do |room|
+    puts room
+  end
+  
 end
